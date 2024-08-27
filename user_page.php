70fda45c55aff1f9d69de740609bbee3a49ccd1e
mknskies11 <161273711+mknskies11@users.noreply.github.com>
2024-08-27 17:28:58 +0530

    Main

---
+++ user_page.php
@@ -0,0 +1,23 @@
+
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Page</title>
+    <link rel="stylesheet" href="backend.css">
+</head>
+<body>
+<div class="admin-container">
+        <div class="admin-content">
+            <h3>Hey there,</h3>
+            <h1>Welcome!</h1>
+            <p>Welcome or welcome back!</p>
+            <a href="#" class="admin-btn">Start Browsing</a>
+            <a href="#" class="admin-btn">Logout</a>
+
+        </div>
+    </div>
+</body>
+</html>
